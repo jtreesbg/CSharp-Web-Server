ca39a3dae63205993938301688dc8737010169eb
jtreesbg <alexandermstefanov@gmail.com>
2021-06-01 18:26:04 +0300

    Created web server and implemented browser connectivity.

---
+++ MyWebServer.Server/HttpServer.cs
@@ -0,0 +1,104 @@
+﻿namespace MyWebServer.Server
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MyWebServer.Server.Http;
+
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener listener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            this.listener = new TcpListener(this.ipAddress, this.port);
+        }
+
+        public async Task Start()
+        {
+            this.listener.Start();
+
+            Console.WriteLine($"Server started on port {port}");
+
+            while (true)
+            {
+
+                var connection = await this.listener.AcceptTcpClientAsync();
+
+                var networkStream = connection.GetStream();
+
+                var requestText = await this.ReadRequest(networkStream);
+
+                Console.WriteLine(requestText);
+
+                // var request = HttpRequest.Parse(requestText);
+
+                await WriteResponse(networkStream);
+
+                connection.Close();
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            var totalBytes = 0;
+
+            var requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+            while (networkStream.DataAvailable);
+
+            return requestBuilder.ToString();
+        }
+
+        private async Task WriteResponse(NetworkStream networkStream)
+        {
+            var content = @"
+<html>
+    <head>
+        <link rel=""icon"" href=""data:,"">
+    </head>
+    <body>
+        Hello from me! ЯША БЕЙБИИИ
+    </body>
+</html>";
+
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"
+HTTP/1.1 200 OK
+Server: My Web Server
+Date: {DateTime.UtcNow:r}
+Content-Length: {contentLength}
+Content-Type: text/html; charset=UTF-8
+
+{content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await networkStream.WriteAsync(responseBytes);
+        }
+    }
+}
